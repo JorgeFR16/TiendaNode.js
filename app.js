f9cddecea4296e6a96526bba014d4ed41961333b
jorge.flores <jorgefr_1693@hotmail.com>
2023-08-23 14:46:30 -0600

    Se implementan algunos Engine Templates

--- app.js
+++ app.js
@@ -2,32 +2,25 @@ const path = require('path');
 
 const express = require('express');
 
-// const bodyParser = require('body-parser');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-// app.use('/', (res, req, next) => {
-//   console.log('This always runs!');
-//   next();
-// });
-
-
-// urlencoded es un parseador del body de la req
-
-app.use(express.urlencoded({extended: true})); // aqui podria ser bodyParser.urlencoded({extended: false}); esto esta haciendo uso de una tercera libreria mientras que con express viene incluido pero lo podria llegar a quitar en algun momento
+app.use(express.urlencoded({extended: true})); 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => { // maneja todas las rutas porque el '/? esta implicito
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.use((req, res, next) => { 
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  // console.log('In another middlewere');
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', { prods: products, pageTitle:'Shop', path:'/', hasProducts: products.length > 0, activeShop: true, productCSS: true });
 }); 
 
 module.exports = router;
