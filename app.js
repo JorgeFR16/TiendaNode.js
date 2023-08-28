a0baa92e43f8e27662c2ef114ca671516aef1f2b
jorge.flores <jorgefr_1693@hotmail.com>
2023-08-28 10:27:41 -0600

    Modificando las rutas, agregando y quitando productos

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.get('/favicon.ico', (req, res) => res.status(204));
@@ -16,12 +18,10 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => { 
-  res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
 
