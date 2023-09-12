d3eb4d3790a42b3833cc3c862063f6103e3d3ba4
jorge.flores <jorgefr_1693@hotmail.com>
2023-09-12 11:21:32 -0600

    Agregando mejoras para la experiencia de usuario y mejoras en las validaciones

--- routes/admin.js
+++ routes/admin.js
@@ -1,25 +1,55 @@
 const path = require('path');
 
 const express = require('express');
+const { body } = require('express-validator');
 
 const adminController = require('../controllers/admin');
-const isauth = require('../middleware/is-auth');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', isauth, adminController.getAddProduct);
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
 // /admin/add-product => GET
-router.get('/products', isauth, adminController.getProducts);
+router.get('/products', isAuth, adminController.getProducts);
 
 // // /admin/add-product => POST
-router.post('/add-product', isauth, adminController.postAddProduct);
+router.post(
+  '/add-product',
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('imageUrl')
+      .isURL(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim(),
+    ],
+  isAuth,
+  adminController.postAddProduct
+);
 
-router.get('/edit-product/:productId', isauth, adminController.getEditProduct);
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isauth, adminController.postEditProduct);
+router.post('/edit-product', 
+[
+  body('title')
+    .isString()
+    .isLength({ min: 3 })
+    .trim(),
+  body('imageUrl')
+    .isURL(),
+  body('price').isFloat(),
+  body('description')
+    .isLength({ min: 5, max: 400 })
+    .trim(),
+  ], 
+  isAuth, adminController.postEditProduct);
 
-router.post('/delete-product', isauth, adminController.postDeleteProduct);
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
